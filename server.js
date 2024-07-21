4d147731f0d0315b7333d8b9dcf2a4906b3e9d97
Beknur <106554639+BEknUur@users.noreply.github.com>
2024-07-22 00:15:32 +0500

    kak zhe ia zaebalsa

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ const typeOfPerson = (
   "You are a bot that generates SAT problems. " +
   "You should write answers for them as the options A, B, C, D."
 );
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
 
 async function generateSatProblems(section, difficulty) {
   try {
